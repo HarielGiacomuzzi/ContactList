7a66f1e523375894227695a7da3ef3619dee66a9
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2019-06-03 11:15:40 -0300

    Add contact service getAll test

--- ContactListTests/ContactListTests.swift
+++ ContactListTests/ContactListTests.swift
@@ -10,18 +10,21 @@ import XCTest
 @testable import ContactList
 
 class ContactListTests: XCTestCase {
+    private var contactService: ContactsService?
+
 
     override func setUp() {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        self.contactService = ContactsService()
     }
 
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testExample() {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testGetAllContacts() {
+        self.contactService?.getAllContacts { contacts in
+            assert(contacts.count != 0)
+        }
     }
 
     func testPerformanceExample() {
